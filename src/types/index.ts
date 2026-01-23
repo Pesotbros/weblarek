d74ac74183c296f81b7a445a3e3c2bf4fca9b501
Pesotbros <53221802+Pesotbros@users.noreply.github.com>
2026-01-23 07:53:27 +0500

    Update index.ts

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,6 +1,6 @@
 export type ApiPostMethods = 'POST' | 'PUT' | 'DELETE';
 
-export type TPayment = 'card' | 'cash' | null
+export type TPayment = 'card' | 'cash' | ''
 
 export interface IApi {
     get<T extends object>(uri: string): Promise<T>;
